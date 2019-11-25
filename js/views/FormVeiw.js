a7631a196aec6a3b9837d61c79b0aee06f2b9064
SungHyun Yu <ysh1513@naver.com>
2019-11-25 22:06:27 +0900

    x버튼 자동생성+reset

--- js/views/FormVeiw.js
+++ js/views/FormVeiw.js
@@ -9,10 +9,20 @@ FormView.setup = function(el){
   this.inputEL = el.querySelector('[type=text]')
   this.resetEL = el.querySelector('[type=reset]')
   this.showResetBtn(false)
+  this.bindEvents()
+  return this
 }
 
 FormView.showResetBtn = function(show=true){
   this.resetEL.style.display = show ? 'block':'none';
+}
+
+FormView.bindEvents = function(){
+  this.inputEL.addEventListener('keyup',e=>this.onKeyup(e))
+}
+
+FormView.onKeyup = function(){
+  this.showResetBtn(this.inputEL.value.length)
 }
 
 export default FormView
