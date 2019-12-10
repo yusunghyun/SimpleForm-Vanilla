ffba14b0033842e986cd7dd8593fcd7cd91cb447
SungHyun Yu <ysh1513@naver.com>
2019-12-10 23:24:36 +0900

    탭클릭할수있게만 만들고 인풋비었을 시 검색결과도 자동 삭제

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,7 @@
 import FormView from '../views/FormVeiw.js'
 import ResultView from '../views/ResultVeiw.js'
 import SearchModel from '../models/SearchModel.js'
+import TabView from '../views/TabView.js'
 
 const tag = '[MainController]'
 
@@ -10,9 +11,22 @@ export default{
       .on('@submit',e=>this.onSubmit(e.detail.input))//이건 나중에 구현?
       .on('@reset',e=>this.onResetForm())//이건 나중에 구현?
       
+    TabView.setup(document.querySelector('#tabs'))
+      .on('@change', e => this.onChangeTab(e.detail.tabName))
+
     ResultView.setup(document.querySelector('#search-result'))
 
+    this.seletedTab = '추천 검색어';//this는 컨트롤?
+    this.renderView()
+    
   },
+  
+  renderView(){
+    console.log(tag,'renderView()')
+    TabView.setActiveTab(this.seletedTab)
+    ResultView.hide()
+  },
+
   search(query){
     console.log(tag,'search()',query)
     SearchModel.list(query).then(data=>{
@@ -29,6 +43,11 @@ export default{
   },
   onResetForm(){
     console.log(tag,'onReset()')
+    ResultView.hide()//hide하고 다시 원상복귀가 안되는데?
   },
+
+  onChangeTab(tabName){
+    debugger
+  }
 
 }
--- js/views/ResultVeiw.js
+++ js/views/ResultVeiw.js
@@ -11,10 +11,22 @@ ResultView.setup = function (el){
 ResultView.render = function(data = [] ){
   console.log(tag,'render()',data);
   this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : "검생결과가 없습니다"
-}
+}//this는 ResultView
 
 ResultView.getSearchResultHtml = function(data){
-  debugger
+  // ResultView.show()//추가함
+  return data.reduce((html,item)=>{
+    html += this.getSearchItemHtml(item)
+    return html
+  },'<ul>')+'</ul>'
+}
+
+ResultView.getSearchItemHtml = function(item){
+  return `<li>
+    <img src = '${item.image}'>
+    <p>${item.name}</p>
+    <li>
+  `
 }
 
 export default ResultView
